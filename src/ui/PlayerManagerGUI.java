f343fd2b9b7031dec119b4315fc58fc2409afc02
Mateo <mateora73@gmail.com>
2021-11-01 15:09:52 -0500

    Organized

--- src/ui/PlayerManagerGUI.java
+++ src/ui/PlayerManagerGUI.java
@@ -42,10 +42,10 @@ public class PlayerManagerGUI {
 	}
 	@FXML
 	private TextField playerToSearch;
-    @FXML
-    private ComboBox<String> searchComboBox;
-    @FXML
-    private ComboBox<String> quicksearchComboBox;
+	@FXML
+	private ComboBox<String> searchComboBox;
+	@FXML
+	private ComboBox<String> quicksearchComboBox;
 	@FXML
 	private ProgressBar bar;
 	@FXML
@@ -117,12 +117,12 @@ public class PlayerManagerGUI {
 
 	@FXML
 	private ListView<Player> lvSearchedPlayers;
-   
+
 	@FXML
 	private GridPane loadingGrid;
-	
+
 	@FXML
-    private TextField quickSearchData;
+	private TextField quickSearchData;
 
 
 	public void initProgram() throws IOException{
@@ -179,204 +179,204 @@ public class PlayerManagerGUI {
 		}
 	}
 
-		    @FXML
-		    void toAddPlayer(ActionEvent event) {
-		    	FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("addPlayer-window.fxml"));
-				Parent userView;
-				try {
+	@FXML
+	void toAddPlayer(ActionEvent event) {
+		FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("addPlayer-window.fxml"));
+		Parent userView;
+		try {
+			userView = fxmlLoader.load();
+			mainPane.getChildren().clear();
+			mainPane.getChildren().add(userView);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	@FXML
+	void toDeletePlayers(ActionEvent event) {
+		FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("deletePlayer-window.fxml"));
+		Parent userView;
+		try {
+			userView = fxmlLoader.load();
+			mainPane.getChildren().clear();
+			mainPane.getChildren().add(userView);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	@FXML
+	void loadPlayers(ActionEvent event) {
+		FileChooser fc = new FileChooser();
+		fc.setTitle("Select the base products list");
+		File f = fc.showOpenDialog(mainPane.getScene().getWindow());
+		try {
+			manager.importData(f.getAbsolutePath());
+		} catch (IOException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+
+		Alert alert = new Alert(AlertType.INFORMATION);
+		alert.setTitle("Import players");
+		alert.setHeaderText("Players have been loaded successfully");
+		alert.showAndWait();
+		FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("main-window.fxml"));
+		fxmlLoader.setController(this);
+		Parent userView;
+		try {
+			userView = fxmlLoader.load();
+			mainPane.getChildren().clear();
+			mainPane.getChildren().add(userView);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	@FXML
+	void toSearchPlayer(ActionEvent event) {
+		FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("searchPlayer-window.fxml"));
+		Parent userView;
+		try {
+			userView = fxmlLoader.load();
+			mainPane.getChildren().clear();
+			mainPane.getChildren().add(userView);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	@FXML
+	void addPlayer(ActionEvent event) {
+		manager.addPlayer(Integer.parseInt(playerId.getText()), playerName.getText(), Integer.parseInt(playerAge.getText()), playerTeam.getText(),Double.parseDouble(playerPoints.getText()), Double.parseDouble(playerRebounds.getText()),Double.parseDouble(playerAssists.getText()), Double.parseDouble(playerSteals.getText()),Double.parseDouble(playerBlocks.getText()));
+	}
+	@FXML
+	void exit(ActionEvent event) {
+		Platform.exit();
+	}
+
+	@FXML
+	void showDeletedList(ActionEvent event) {
+		FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("deletedPlayerList-window.fxml"));
+		Parent userView;
+		try {
+			userView = fxmlLoader.load();
+			mainPane.getChildren().clear();
+			mainPane.getChildren().add(userView);
+			lvDeletedPlayers.setItems(FXCollections.observableArrayList(deletedPlayers));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+	}		   
+
+	@FXML
+	void restorePlayer(ActionEvent event) {
+		Player playerToRestore = lvDeletedPlayers.getSelectionModel().getSelectedItem();
+		manager.addPlayer(playerToRestore);
+
+	}
+
+	@FXML
+	void deletePlayer(ActionEvent event) {
+		Player playerTemp=manager.searchId(Integer.parseInt(playerToDelete.getText()));
+		deletedPlayers.add(playerTemp);
+		manager.removePlayer(manager.searchId(Integer.parseInt(playerToDelete.getText())));
+
+		if(manager.searchId(Integer.parseInt(playerToDelete.getText()))!=null) {
+			Alert alert = new Alert(AlertType.INFORMATION);
+			alert.setTitle("Delete players");
+			alert.setHeaderText("Player "+manager.searchId(Integer.parseInt(playerToDelete.getText()))+" has been deleted successfully");
+			alert.showAndWait();
+			FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("main-window.fxml"));
+			fxmlLoader.setController(this);
+			Parent userView;
+			try {
 				userView = fxmlLoader.load();
 				mainPane.getChildren().clear();
 				mainPane.getChildren().add(userView);
-				} catch (IOException e) {
-					e.printStackTrace();
-				}
-		    }
-
-		    @FXML
-		    void toDeletePlayers(ActionEvent event) {
-		    	FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("deletePlayer-window.fxml"));
-				Parent userView;
-				try {
-				userView = fxmlLoader.load();
-				mainPane.getChildren().clear();
-				mainPane.getChildren().add(userView);
-				} catch (IOException e) {
-					e.printStackTrace();
-				}
-		    }
-
-		    @FXML
-		    void loadPlayers(ActionEvent event) {
-		    	FileChooser fc = new FileChooser();
-		        fc.setTitle("Select the base products list");
-		        File f = fc.showOpenDialog(mainPane.getScene().getWindow());
-		        try {
-					manager.importData(f.getAbsolutePath());
-				} catch (IOException e1) {
-					// TODO Auto-generated catch block
-					e1.printStackTrace();
-				}
-		        
-		        Alert alert = new Alert(AlertType.INFORMATION);
-		        alert.setTitle("Import players");
-		        alert.setHeaderText("Players have been loaded successfully");
-		        alert.showAndWait();
-		        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("main-window.fxml"));
-				fxmlLoader.setController(this);
-				Parent userView;
-				try {
-					userView = fxmlLoader.load();
-					mainPane.getChildren().clear();
-					mainPane.getChildren().add(userView);
-				} catch (IOException e) {
-					e.printStackTrace();
-				}
-		    }
-
-		    @FXML
-		    void toSearchPlayer(ActionEvent event) {
-		    	FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("searchPlayer-window.fxml"));
-				Parent userView;
-				try {
-				userView = fxmlLoader.load();
-				mainPane.getChildren().clear();
-				mainPane.getChildren().add(userView);
-				} catch (IOException e) {
-					e.printStackTrace();
-				}
-		    }
-		    @FXML
-		    void addPlayer(ActionEvent event) {
-		    	manager.addPlayer(Integer.parseInt(playerId.getText()), playerName.getText(), Integer.parseInt(playerAge.getText()), playerTeam.getText(),Double.parseDouble(playerPoints.getText()), Double.parseDouble(playerRebounds.getText()),Double.parseDouble(playerAssists.getText()), Double.parseDouble(playerSteals.getText()),Double.parseDouble(playerBlocks.getText()));
-		    }
-		    @FXML
-		    void exit(ActionEvent event) {
-		    	Platform.exit();
-		    }
-
-		    @FXML
-		    void showDeletedList(ActionEvent event) {
-		    	FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("deletedPlayerList-window.fxml"));
-				Parent userView;
-				try {
-				userView = fxmlLoader.load();
-				mainPane.getChildren().clear();
-				mainPane.getChildren().add(userView);
-				lvDeletedPlayers.setItems(FXCollections.observableArrayList(deletedPlayers));
-				} catch (IOException e) {
-					e.printStackTrace();
-				}
-				
-		    }		   
-
-		    @FXML
-		    void restorePlayer(ActionEvent event) {
-		    	Player playerToRestore = lvDeletedPlayers.getSelectionModel().getSelectedItem();
-		    	manager.addPlayer(playerToRestore);
-		    	
-		    }
-		    
-		    @FXML
-		    void deletePlayer(ActionEvent event) {
-		    	Player playerTemp=manager.searchId(Integer.parseInt(playerToDelete.getText()));
-		    	deletedPlayers.add(playerTemp);
-		    	manager.removePlayer(manager.searchId(Integer.parseInt(playerToDelete.getText())));
-		    	
-		    	if(manager.searchId(Integer.parseInt(playerToDelete.getText()))!=null) {
-		    		Alert alert = new Alert(AlertType.INFORMATION);
-			        alert.setTitle("Delete players");
-			        alert.setHeaderText("Player "+manager.searchId(Integer.parseInt(playerToDelete.getText()))+" has been deleted successfully");
-			        alert.showAndWait();
-			        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("main-window.fxml"));
-					fxmlLoader.setController(this);
-					Parent userView;
-					try {
-						userView = fxmlLoader.load();
-						mainPane.getChildren().clear();
-						mainPane.getChildren().add(userView);
-					} catch (IOException e) {
-						e.printStackTrace();
-					}
-		    	}else {
-		    		Alert alert = new Alert(AlertType.INFORMATION);
-			        alert.setTitle("Delete players");
-			        alert.setHeaderText("Player not found");
-			        alert.showAndWait();
-		    	}
-		    }
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}else {
+			Alert alert = new Alert(AlertType.INFORMATION);
+			alert.setTitle("Delete players");
+			alert.setHeaderText("Player not found");
+			alert.showAndWait();
+		}
+	}
 
 
-		    @FXML
-		    void searchPlayer(ActionEvent event) {
-		    int selection=0;
-		    searchComboBox.getItems().addAll("id", "name", "age", "team");
-		    if(searchComboBox.getValue().equals("id")) {
-		    	selection = 1;
-		    }else if(searchComboBox.getValue().equals("name")) {
-		    	selection = 2;
-		    }else if(searchComboBox.getValue().equals("age")) {
-		    	selection = 3;
-		    }else if(searchComboBox.getValue().equals("team")) {
-		    	selection = 4;
-		    }
-		    
-		    
-		    	switch(selection){
-		    	case 1:
-		    		manager.searchId(Integer.parseInt(playerToSearch.getText()));
-		    		break;
-		    	case 2:
-		    		manager.searchName(playerToSearch.getText());
-		    		break;
-		    	case 3:
-		    		manager.searchAge(Integer.parseInt(playerToSearch.getText()));
-		    		break;
-		    	case 4: 
-		    		manager.searchTeam(playerToSearch.getText());
-		    		break;
-		    	default:
-		    		break;
-		    	}
-		    }
+	@FXML
+	void searchPlayer(ActionEvent event) {
+		int selection=0;
+		searchComboBox.getItems().addAll("id", "name", "age", "team");
+		if(searchComboBox.getValue().equals("id")) {
+			selection = 1;
+		}else if(searchComboBox.getValue().equals("name")) {
+			selection = 2;
+		}else if(searchComboBox.getValue().equals("age")) {
+			selection = 3;
+		}else if(searchComboBox.getValue().equals("team")) {
+			selection = 4;
+		}
 
-		    @FXML
-		    void quickSearch(ActionEvent event) {
-		    	int selection=0;
-		    	quicksearchComboBox.getItems().addAll("points", "rebounds", "assists", "steals", "blocks");
-		    	 if(quicksearchComboBox.getValue().equals("points")) {
-				    	selection = 1;
-				    }else if(quicksearchComboBox.getValue().equals("rebounds")) {
-				    	selection = 2;
-				    }else if(quicksearchComboBox.getValue().equals("assists")) {
-				    	selection = 3;
-				    }else if(quicksearchComboBox.getValue().equals("steals")) {
-				    	selection = 4;
-				    }else if(quicksearchComboBox.getValue().equals("blocks")) {
-				    	selection = 5;
-				    }
-		    	switch(selection){
-		    	case 1:
-		    		manager.quickSearch(selection,Double.parseDouble(quickSearchData.getText()));
-		    		break;
-		    	case 2:
-		    		manager.quickSearch(selection,Double.parseDouble(quickSearchData.getText()));
-		    		break;
-		    	case 3:
-		    		manager.quickSearch(selection,Double.parseDouble(quickSearchData.getText()));
-		    		break;
-		    	case 4: 
-		    		manager.quickSearch(selection,Double.parseDouble(quickSearchData.getText()));
-		    		break;
-		    	case 5:
-		    		manager.quickSearch(selection,Double.parseDouble(quickSearchData.getText()));
-		    		break;
-		    	default:
-		    		break;
-		    		
-		    	}
-		    }
 
-		
-	} 
-	
+		switch(selection){
+		case 1:
+			manager.searchId(Integer.parseInt(playerToSearch.getText()));
+			break;
+		case 2:
+			manager.searchName(playerToSearch.getText());
+			break;
+		case 3:
+			manager.searchAge(Integer.parseInt(playerToSearch.getText()));
+			break;
+		case 4: 
+			manager.searchTeam(playerToSearch.getText());
+			break;
+		default:
+			break;
+		}
+	}
+
+	@FXML
+	void quickSearch(ActionEvent event) {
+		int selection=0;
+		quicksearchComboBox.getItems().addAll("points", "rebounds", "assists", "steals", "blocks");
+		if(quicksearchComboBox.getValue().equals("points")) {
+			selection = 1;
+		}else if(quicksearchComboBox.getValue().equals("rebounds")) {
+			selection = 2;
+		}else if(quicksearchComboBox.getValue().equals("assists")) {
+			selection = 3;
+		}else if(quicksearchComboBox.getValue().equals("steals")) {
+			selection = 4;
+		}else if(quicksearchComboBox.getValue().equals("blocks")) {
+			selection = 5;
+		}
+		switch(selection){
+		case 1:
+			manager.quickSearch(selection,Double.parseDouble(quickSearchData.getText()));
+			break;
+		case 2:
+			manager.quickSearch(selection,Double.parseDouble(quickSearchData.getText()));
+			break;
+		case 3:
+			manager.quickSearch(selection,Double.parseDouble(quickSearchData.getText()));
+			break;
+		case 4: 
+			manager.quickSearch(selection,Double.parseDouble(quickSearchData.getText()));
+			break;
+		case 5:
+			manager.quickSearch(selection,Double.parseDouble(quickSearchData.getText()));
+			break;
+		default:
+			break;
+
+		}
+	}
+
+
+} 
+
 
